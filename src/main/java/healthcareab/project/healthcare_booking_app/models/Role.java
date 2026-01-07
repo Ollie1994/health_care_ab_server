b43c9fa1114793410cdf2892db7d8b90c7084044
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2026-01-07 10:17:46 +0100

    added comment

--- src/main/java/healthcareab/project/healthcare_booking_app/models/Role.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/models/Role.java
@@ -1,5 +1,7 @@
 package healthcareab.project.healthcare_booking_app.models;
 
+// Test comment for test branch protection
+// Change Admin to Caretaker and change User to Patient
 public enum Role {
     USER,
     ADMIN
