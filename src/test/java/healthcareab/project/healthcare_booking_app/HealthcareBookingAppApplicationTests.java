7e7355c0af0bcb80050dcbe301cc97447cfcc401
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2026-01-08 12:39:28 +0100

    remove @dis test

--- src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
+++ src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
@@ -1,11 +1,9 @@
 package healthcareab.project.healthcare_booking_app;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled // REMOVE IF WANT INTEGRATION TESTS
 class HealthcareBookingAppApplicationTests {
 
 	@Test
