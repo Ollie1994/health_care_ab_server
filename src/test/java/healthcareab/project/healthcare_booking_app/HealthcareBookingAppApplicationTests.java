fab639b6b648ceb8cc86c37c6901743a5d822496
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2026-01-08 10:08:44 +0100

    jacoco plugin

--- src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
+++ src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-// REMOVE AFTER ADDING TESTS
+// REMOVE LATER
 @Disabled
 class HealthcareBookingAppApplicationTests {
 
