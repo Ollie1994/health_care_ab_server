3746cc36a86a20dfe104345a1cbed81053a181ca
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2026-01-08 11:53:46 +0100

    report

--- src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
+++ src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
@@ -1,9 +1,11 @@
 package healthcareab.project.healthcare_booking_app;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled // REMOVE IF WANT INTEGRATION TESTS
 class HealthcareBookingAppApplicationTests {
 
 	@Test
