f6bcbd0a73586dfd972d898810c7b3f226d987f9
KarolinaSvensson <karolina@yohrs.com>
2026-01-12 18:14:42 +0100

    added PatchBookingResponse and method to BookingService

--- src/main/java/healthcareab/project/healthcare_booking_app/converters/BookingConverter.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/converters/BookingConverter.java
@@ -1,6 +1,7 @@
 package healthcareab.project.healthcare_booking_app.converters;
 
 import healthcareab.project.healthcare_booking_app.dto.CreateBookingResponse;
+import healthcareab.project.healthcare_booking_app.dto.PatchBookingResponse;
 import healthcareab.project.healthcare_booking_app.models.Booking;
 import healthcareab.project.healthcare_booking_app.models.User;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,17 @@ public class BookingConverter {
                 user.getFirstName(),
                 booking.getStart_date_time(),
                 booking.getEnd_date_time()
+        );
+    }
+
+    public PatchBookingResponse convertToPatchBookingResponse(Booking booking, User user) {
+        return new PatchBookingResponse(
+                booking.getId(),
+                user.getFirstName(),
+                booking.getStatus(),
+                booking.getStart_date_time(),
+                booking.getEnd_date_time(),
+                "Booking has been cancelled successfully"
         );
     }
 }
--- src/main/java/healthcareab/project/healthcare_booking_app/services/BookingService.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/services/BookingService.java
@@ -3,13 +3,19 @@ package healthcareab.project.healthcare_booking_app.services;
 import healthcareab.project.healthcare_booking_app.converters.BookingConverter;
 import healthcareab.project.healthcare_booking_app.dto.CreateBookingRequest;
 import healthcareab.project.healthcare_booking_app.dto.CreateBookingResponse;
+import healthcareab.project.healthcare_booking_app.dto.PatchBookingResponse;
+import healthcareab.project.healthcare_booking_app.exceptions.ConflictException;
 import healthcareab.project.healthcare_booking_app.exceptions.ResourceNotFoundException;
+import healthcareab.project.healthcare_booking_app.exceptions.UnauthorizedException;
 import healthcareab.project.healthcare_booking_app.models.Booking;
 import healthcareab.project.healthcare_booking_app.models.BookingStatus;
 import healthcareab.project.healthcare_booking_app.models.User;
 import healthcareab.project.healthcare_booking_app.repository.BookingRepository;
 import healthcareab.project.healthcare_booking_app.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Service
 public class BookingService {
@@ -48,5 +54,47 @@ public class BookingService {
         return bookingConverter.convertToCreateBookingResponse(createdBooking, caregiver);
     }
 
+    public PatchBookingResponse cancelBooking(String bookingId) {
+
+        // Get booking by booking id or else throw exception
+        Booking booking = bookingRepository.findById(bookingId).orElseThrow(() -> new ResourceNotFoundException("Booking not found"));
+
+        // Get current logged-in user (patient)
+        User patient = authService.getAuthenticated();
+
+        // Check if current user (patient) is same as patient in booking
+        if (!booking.getPatient_id().equals(authService.getAuthenticated().getId())) {
+            throw new UnauthorizedException("You do not have permission to cancel this booking");
+        }
+
+        // Throw exception if current status of booking is already cancelled
+        if (booking.getStatus() == BookingStatus.CANCELLED) {
+            throw new ConflictException("This booking has already been cancelled");
+        }
+
+        // Check if booking date has already been
+        if (booking.getEnd_date_time().isBefore(LocalDateTime.now())) {
+            throw new ConflictException("This booking has already passed");
+        }
+
+        // Check if there's a minimum of 24 hours before the appointment
+        Duration durationUntilStart = Duration.between(LocalDateTime.now(), booking.getStart_date_time());
+
+        if (durationUntilStart.toHours() < 24) {
+    throw new IllegalArgumentException("Booking cannot be cancelled within 24 hours of start time");
+
+        }
+
+        // Get caregiver
+        User caregiver = userRepository.findById(booking.getCaregiver_id())
+                .orElseThrow(() -> new ResourceNotFoundException("Caregiver not found"));
+
+        // Cancel booking and return DTO
+        booking.setStatus(BookingStatus.CANCELLED);
+
+        Booking createdBooking = bookingRepository.save(booking);
+        return bookingConverter.convertToPatchBookingResponse(createdBooking, caregiver);
+
+    }
 
 }
