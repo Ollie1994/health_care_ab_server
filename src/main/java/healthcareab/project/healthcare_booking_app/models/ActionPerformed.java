a23e8db9f89cb68bf3b5856e8e00ba235e89a217
SatangSagwa <emilia.ivarsson@live.se>
2026-01-16 17:45:47 +0100

    created model Log with all fields, contructor and getters. Created ENUM for ActionPerformed.

---
+++ src/main/java/healthcareab/project/healthcare_booking_app/models/ActionPerformed.java
@@ -0,0 +1,15 @@
+package healthcareab.project.healthcare_booking_app.models;
+
+public enum ActionPerformed {
+    viewedProfile,
+    viewedBooking,
+    createdBooking,
+    createdAccount,
+    editedBooking,
+    cancelledBooking,
+    editedAccount,
+    anonymizedAccount,
+    bannedAccount,
+    authorization,
+    authentication
+}
