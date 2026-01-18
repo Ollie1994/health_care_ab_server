9c2949afa1a8a2080aaa79ce2c2325ebd6f20e72
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2026-01-18 14:29:29 +0100

    All validation checked, all shoudl work now

--- src/main/java/healthcareab/project/healthcare_booking_app/validators/period/PeriodValidator.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/validators/period/PeriodValidator.java
@@ -10,6 +10,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +27,47 @@ public class PeriodValidator {
 
         LocalDate today = LocalDate.now();
 
+        // Standard format validation
+        if (request.getEndDateTime() == null || request.getStartDateTime() == null || request.getCaregiverId() == null) {
+            throw new IllegalArgumentException("Request cant have null values");
+        }
+        if (request.getStartDateTime().isAfter(request.getEndDateTime())) {
+            throw new IllegalArgumentException("Period start date should be before end date");
+        }
+        if (request.getStartDateTime().isEqual(request.getEndDateTime())) {
+            throw new IllegalArgumentException("Period start date cant be equal to end date");
+        }
+        if (request.getStartDateTime().toLocalDate().isBefore(today) || request.getEndDateTime().toLocalDate().isBefore(today)) {
+            throw new IllegalArgumentException("Period cant be before today's date");
+        }
+
+        // Validation for how far ahead you can set availability
+        long daysAhead = ChronoUnit.DAYS.between(today, request.getStartDateTime().toLocalDate());
+        if (daysAhead > 28) {
+            throw new IllegalArgumentException("Period cant be added that is more than 28 days in the future");
+        }
+
+        // Validation for availability length
+        Duration duration = Duration.between(request.getStartDateTime(), request.getEndDateTime());
+        if (duration.toMinutes() > 60) {
+            throw new IllegalArgumentException("Period cant be longer than 1 hour");
+        }
+        if (duration.toMinutes() < 60) {
+            throw new IllegalArgumentException("Period cant be shorter than 1 hour");
+        }
+
+        // validation for lunch time
+        LocalTime start = request.getStartDateTime().toLocalTime();
+        LocalTime end = request.getEndDateTime().toLocalTime();
+        if (start.compareTo(LocalTime.NOON) >= 0 && start.compareTo(LocalTime.of(13, 0)) < 0) {
+            throw new IllegalArgumentException("Start time cannot be during lunch (12:00–13:00)");
+        }
+        if (end.compareTo(LocalTime.NOON) > 0 && end.compareTo(LocalTime.of(13, 0)) < 0) {
+            throw new IllegalArgumentException("End time cannot be during lunch (12:00–13:00)");
+        }
+
+
+        // Validate new period against existing periods
         if (periodIds != null && !periodIds.isEmpty() && periodIds.get(0) != null) {
 
             List<Period> periods = new ArrayList<>();
@@ -38,56 +80,24 @@ public class PeriodValidator {
             }
 
             for (Period p : periods) {
-                LocalDateTime start = p.getStartDateTime();
-                LocalDateTime end = p.getEndDateTime();
-                if (request.getStartDateTime().equals(start) || request.getEndDateTime().equals(end)) {
+                LocalDateTime startDateTime = p.getStartDateTime();
+                LocalDateTime endDateTime = p.getEndDateTime();
+                if (request.getStartDateTime().equals(startDateTime) || request.getEndDateTime().equals(endDateTime)) {
                     throw new IllegalArgumentException("Period already exists");
                 }
-                if (Duration.between(end, request.getStartDateTime()).abs().toMinutes() < 10
-                        || Duration.between(start, request.getEndDateTime()).abs().toMinutes() < 10) {
-                    throw new IllegalArgumentException("NewPeriod cant be scheduled within 10 minutes of another meeting");
+                if (Duration.between(endDateTime, request.getStartDateTime()).abs().toMinutes() < 10
+                        || Duration.between(startDateTime, request.getEndDateTime()).abs().toMinutes() < 10) {
+                    throw new IllegalArgumentException("Period cant be scheduled within 10 minutes of another meeting");
                 }
-                if (request.getStartDateTime().isBefore(start) && request.getEndDateTime().isBefore(end) && request.getEndDateTime().isAfter(start)) {
-                    throw new IllegalArgumentException("NewPeriod cant be within an already existing period");
+                if (request.getStartDateTime().isBefore(startDateTime) && request.getEndDateTime().isBefore(endDateTime) && request.getEndDateTime().isAfter(startDateTime)) {
+                    throw new IllegalArgumentException("Period cant be within an already existing period");
                 }
-                if (request.getStartDateTime().isAfter(start) && request.getEndDateTime().isAfter(end) && request.getStartDateTime().isBefore(end)) {
-                    throw new IllegalArgumentException("NewPeriod cant be within an already existing period");
+                if (request.getStartDateTime().isAfter(startDateTime) && request.getEndDateTime().isAfter(endDateTime) && request.getStartDateTime().isBefore(endDateTime)) {
+                    throw new IllegalArgumentException("Period cant be within an already existing period");
                 }
             }
         }
-
-        LocalTime start = request.getStartDateTime().toLocalTime();
-        LocalTime end = request.getEndDateTime().toLocalTime();
-        Duration duration = Duration.between(request.getStartDateTime(), request.getEndDateTime());
-
-        if (start.compareTo(LocalTime.NOON) >= 0 && start.compareTo(LocalTime.of(13, 0)) < 0) {
-            throw new IllegalArgumentException("Start time cannot be during lunch (12:00–13:00)");
-        }
-        if (end.compareTo(LocalTime.NOON) > 0 && end.compareTo(LocalTime.of(13, 0)) < 0) {
-            throw new IllegalArgumentException("End time cannot be during lunch (12:00–13:00)");
-        }
-        if (duration.toMinutes() > 60) {
-            throw new IllegalArgumentException("NewPeriod cant be longer than 1 hour");
-        }
-        if (duration.toMinutes() < 60) {
-            throw new IllegalArgumentException("NewPeriod cant be shorter than 1 hour");
-        }
-        if (request.getEndDateTime() == null || request.getStartDateTime() == null) {
-            throw new IllegalArgumentException("New period cant have null values");
-        }
-        if (request.getStartDateTime().toLocalDate().isBefore(today) || request.getEndDateTime().toLocalDate().isBefore(today)) {
-            throw new IllegalArgumentException("Period cant be before today's date");
-        }
-        if (request.getStartDateTime().isAfter(request.getEndDateTime())) {
-            throw new IllegalArgumentException("New period start date should be before end date");
-        }
-        if (request.getStartDateTime().isEqual(request.getEndDateTime())) {
-            throw new IllegalArgumentException("New period start date cant be equal to end date");
-        }
-        java.time.Period result = java.time.Period.between(today, request.getStartDateTime().toLocalDate());
-        if (result.getDays() > 28) {
-            throw new IllegalArgumentException("New period cant be added that is more than 28 days in the future");
-        }
     }
+
 
 }
